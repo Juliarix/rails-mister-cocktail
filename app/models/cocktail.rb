362bee33d830f209e0376d0152244289f0d06058
Julia Rix <Juliarix@gmail.com>
2017-11-03 12:35:11 -0400

    Uploaded photo and edited simple form

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,5 @@
 class Cocktail < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   validates :name, uniqueness: true, presence: true, allow_blank: false
 
   has_many :doses, dependent: :destroy
